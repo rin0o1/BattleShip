15e2197de8a59deb3f05945161db5f9ad4f15701
FRANCESCO RINALDI <s23100@iisve.it>
2020-12-23 09:11:06 +0100

    StartView

--- core/src/com/battleship/game/BattleShip.java
+++ core/src/com/battleship/game/BattleShip.java
@@ -1,33 +1,65 @@
 package com.battleship.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BattleShip extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class BattleShip extends Game {
+	static public Skin gameSkin;
+	public void create () {
+		String path= "C:\\Users\\Admin\\OneDrive - Brunel University London\\Desktop\\Tests\\skin\\comic-ui.json";
+		gameSkin = new Skin(Gdx.files.internal(path));
+		this.setScreen(new StartView(this));
+	}
+
+	public void render () {
+		super.render();
+	}
+
+	public void dispose () {
+	}
+}
+
+/*public class Test extends ApplicationAdapter {
+	private Stage stage;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage(new ScreenViewport());
+
+		Texture texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+
+		Image image1 = new Image(texture);
+		image1.setPosition(Gdx.graphics.getWidth()/3-image1.getWidth()/2,Gdx.graphics.getHeight()*2/3-image1.getHeight()/2);
+		stage.addActor(image1);
+
+		Image image2 = new Image(texture);
+		image2.setPosition(Gdx.graphics.getWidth()*2/3-image2.getWidth()/2,Gdx.graphics.getHeight()*2/3-image2.getHeight()/2);
+		image2.setOrigin(image2.getWidth()/2,image2.getHeight()/2);
+		image2.rotateBy(45);
+		stage.addActor(image2);
+
+		Image image3 = new Image(texture);
+		image3.setSize(texture.getWidth()/2,texture.getHeight()/2);
+		image3.setPosition(Gdx.graphics.getWidth()/3-image3.getWidth()/2,Gdx.graphics.getHeight()/3-image3.getHeight());
+		stage.addActor(image3);
+
+		texture.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
+		TextureRegion textureRegion = new TextureRegion(texture);
+		textureRegion.setRegion(0,0,texture.getWidth()*8,texture.getHeight()*4);
+		Image image4 = new Image(textureRegion);
+		image4.setSize(200,100);
+		image4.setPosition(Gdx.graphics.getWidth()*2/3-image4.getWidth()/2,Gdx.graphics.getHeight()/3-image4.getHeight());
+		stage.addActor(image4);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
-}
+		stage.act();
+		stage.draw();
+
+	}*/
---
+++ core/src/com/battleship/game/StartView.java
@@ -0,0 +1,107 @@
+package com.battleship.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class StartView implements Screen {
+
+    private Stage stage;
+    private Game game;
+
+    public StartView(Game agame){
+        game = agame;
+        stage = new Stage(new ScreenViewport());
+        Label title = new Label("BATTLESHIP", BattleShip.gameSkin);
+        title.setAlignment(Align.center);
+        title.setY(Gdx.graphics.getHeight()*2/3);
+        title.setWidth(Gdx.graphics.getWidth());
+        stage.addActor(title);
+
+        TextButton playButton = new TextButton("Single player",BattleShip.gameSkin);
+        playButton.setWidth(Gdx.graphics.getWidth()/2);
+        playButton.setPosition(
+                Gdx.graphics.getWidth()/2-playButton.getWidth()/2,
+                Gdx.graphics.getHeight()/2-playButton.getHeight()/2);
+
+        playButton.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event,
+                                 float x,
+                                 float y,
+                                 int pointer,
+                                 int button)
+            {
+                //   game.setScreen(new GameScreen(game));
+            }
+
+        });
+        stage.addActor(playButton);
+
+
+        TextButton optionsButton = new TextButton("Settings",BattleShip.gameSkin);
+        optionsButton.setWidth(Gdx.graphics.getWidth()/2);
+        optionsButton.setPosition(
+                Gdx.graphics.getWidth()/2-optionsButton.getWidth()/2,
+                Gdx.graphics.getHeight()/4-optionsButton.getHeight()/2);
+        optionsButton.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event,
+                                 float x,
+                                 float y,
+                                 int pointer,
+                                 int button)
+            {
+                //  game.setScreen(new OptionScreen(game));
+            }
+        });
+        stage.addActor(optionsButton);
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
